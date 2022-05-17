51a1c37db3904ec788182e938e6ceef8aaf16cf2
Ignacio Espinosa <emprendiendoem@gmail.com>
2022-05-17 14:08:55 -0400

    Default admin false añadido a tabla de users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2019_03_01_151858) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_180659) do
   create_table "stories", force: :cascade do |t|
     t.string "title"
     t.string "picture"
@@ -30,7 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2019_03_01_151858) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "name"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
